228db91f8b3e9ee9c0b2f9ed9409089e7c96839a
Arash Jazi <Jazi@MacBook-Pro-126.local>
2025-04-25 19:07:01 +0200

    publication titles

---
+++ src/sanity/schemaTypes/publication.ts
@@ -0,0 +1,40 @@
+// ./schemas/publication.ts
+import { defineType } from "sanity";
+
+export default defineType({
+  name: "publication",
+  title: "Publications",
+  type: "document",
+  fields: [
+    {
+      name: "title",
+      title: "Title",
+      type: "string",
+      validation: Rule => Rule.required(),
+    },
+    {
+      name: "slug",
+      title: "Slug",
+      type: "slug",
+      options: {
+        source: "title",
+        maxLength: 96,
+      },
+      validation: Rule => Rule.required(),
+    },
+    {
+      name: "cover",
+      title: "Cover Image",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: "description",
+      title: "Description",
+      type: "array",
+      of: [{ type: "block" }],
+    },
+  ],
+});
