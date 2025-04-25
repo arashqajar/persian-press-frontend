809d24372653c8ef988a3c4f9b5d4fdc1e9a030e
Arash Jazi <Jazi@MacBook-Pro-126.local>
2025-04-25 14:30:34 +0200

    fix: correct postcss config format for Next.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,7 @@
 // postcss.config.js
 module.exports = {
-  plugins: {
-    // reference by name, not by importing the function
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+  plugins: [
+    'tailwindcss',
+    'autoprefixer',
+  ],
 };
