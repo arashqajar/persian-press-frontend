08952d673fbd805f21dd91afe5d65d771c03c082
Arash Jazi <Jazi@MacBook-Pro-126.local>
2025-04-25 10:53:55 +0200

    Fix build: remove missing about.ts import

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -1,7 +1,6 @@
 import { type SchemaTypeDefinition } from 'sanity'
-import about from './about'
 import staticPage from './staticPage'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [about, staticPage],
+  types: [staticPage],
 }
